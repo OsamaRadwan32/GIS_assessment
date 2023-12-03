3d4303a6b14d615f8579cec4b0ed9085b38b7b7f
Osama Radwan <osamaradwan46@gmail.com>
2023-12-03 13:23:32 +0200

    Implemented user_routes

--- app/routes/users_routes.py
+++ app/routes/users_routes.py
@@ -1,31 +1,17 @@
 from flask import Blueprint, jsonify, request
-from ..controllers import user_controller
+from ..controllers.user_controller import UserController
 
 user_routes = Blueprint('user_routes', __name__)
 
-@user_routes.route('/<int:user_id>/', methods=['PUT'])
-def update_row(user_id):
-    # update_row = content_controller.update_row(table_id, row_id) 
-    # return update_row
-    return jsonify({'message': 'Update user route', 'table_id': f'{user_id}'})
+@user_routes.route('/', methods=['GET','POST'])
+def get_all_or_create():
+    if request.method == 'GET': return UserController.get_users()
+    if request.method == 'POST': return UserController.create_user(request)
+    else: return 'Method is Not Allowed'    
 
-@user_routes.route('/delete/<int:user_id>/', methods=['DELETE'])
-def delete_row(user_id):
-    # delete_row = content_controller.delete_row(table_id, row_id) 
-    # return delete_row
-    return jsonify({'message': 'Delete user route', 'user_id': f'{user_id}'})
-
-
-# Boiler plate methods
-@user_routes.route("/accounts", methods=['GET', 'POST'])
-def list_create_accounts():
-    if request.method == 'GET': return list_all_accounts_controller()
-    if request.method == 'POST': return create_account_controller()
-    else: return 'Method is Not Allowed'
-
-@user_routes.route("/accounts/<account_id>", methods=['GET', 'PUT', 'DELETE'])
-def retrieve_update_destroy_accounts(account_id):
-    if request.method == 'GET': return retrieve_account_controller(account_id)
-    if request.method == 'PUT': return update_account_controller(account_id)
-    if request.method == 'DELETE': return delete_account_controller(account_id)
-    else: return 'Method is Not Allowed'
+@user_routes.route('/<int:user_id>/', methods=['GET', 'PUT', 'DELETE'])
+def get_update_delete(user_id):
+    if request.method == 'GET': return UserController.get_user(user_id)
+    if request.method == 'PUT': return UserController.update_user(user_id)
+    if request.method == 'DELETE': return UserController.delete_user(user_id)
+    else: return 'Method is Not Allowed'    
