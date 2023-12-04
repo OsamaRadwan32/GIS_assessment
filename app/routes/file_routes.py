7c3b2538694720b4db78fec7e43f8af449a7cd2d
Osama Radwan <osamaradwan46@gmail.com>
2023-12-04 18:13:54 +0200

    Modified main routes and added file controller process request route

--- app/routes/file_routes.py
+++ app/routes/file_routes.py
@@ -3,6 +3,6 @@ from ..controllers.file_controller import FileController
 
 file_routes = Blueprint('file_routes', __name__)
 
-@file_routes.route('/upload/', methods=['POST'])
+@file_routes.route('/', methods=['POST'])
 def upload_file():
-    return FileController.upload_file(request.file)
+    return FileController.process_request(request)
--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -20,23 +20,6 @@ def index():
 def normal_db_connection():
     return db_connection()
 
-# @main_routes.route('/content')
-# def content():
-#     return jsonify({'message': 'Content page'})
-
-# @main_routes.route('/tables')
-# def get_tables():
-#     return jsonify({'message': 'Tables page'})
-
-# @main_routes.route('/users')
-# def get_users():
-#     return jsonify({'message': 'Users page'})
-
-# @main_routes.route('/auth')
-# def get_users():
-#     return jsonify({'message': 'Auth page'})
-
-main_routes.register_blueprint(content_routes, url_prefix='/content')
 main_routes.register_blueprint(table_routes, url_prefix='/tables')
 main_routes.register_blueprint(user_routes, url_prefix='/users')
 main_routes.register_blueprint(file_routes, url_prefix='/file')
