2728a6093130dd690bd9815c90aa3b8a6d93e503
Osama Radwan <osamaradwan46@gmail.com>
2023-12-04 14:05:25 +0200

    Implemented create_table_record() and create_table() methods in the TableController class

--- app/controllers/table_controller.py
+++ app/controllers/table_controller.py
@@ -13,36 +13,35 @@ class TableController:
     @staticmethod
     def create_table_record(table_name, user_id, structure):
         table_record = Table(
-                            name    = table_name,
-                            user_id = user_id,
+                            name        = table_name,
+                            user_id     = user_id,
+                            structure   = structure
                             )
         db.session.add(table_record)
         db.session.commit()
 
     @staticmethod
-    def create_table(request):
-        if not request.json:
-            return jsonify({'error': 'No data provided'}), 400
+    def create_table(table_name, user_id, structure):
+        
+        table_columns = {
+            'id': db.Column(db.Integer, primary_key=True)
+        }
 
-        user_id = request.json.get('user_id')
-        table_name = request.json.get('table_name')
-        columns = request.json.get('columns')
-        datatypes = request.json.get('datatypes')
-
-        if not columns or not datatypes:
-            return jsonify({'error': 'Columns or datatypes missing'}), 400
-
-        if len(columns) != len(datatypes):
-            return jsonify({'error': 'Mismatch in columns and datatypes'}), 400
+        for col in structure:
+            col_name = col['name']
+            data_type = col['data_type']
+            
+            # Map data types to SQLAlchemy types
+            if data_type == 'integer':
+                column = db.Column(db.Integer)
+            elif data_type == 'string':
+                column = db.Column(db.String(255))  # Replace 255 with desired string length
+            
+            table_columns[col_name] = column
 
         try:
-            metadata = MetaData()
-            new_table = Table(f'{table_name}', metadata, *[Column(col, getattr(Integer, dtype)) for col, dtype in zip(columns, datatypes)])
-            new_table.create(bind=db.engine)
-            TableController.create_table_record(table_name, user_id)
+            table = type(table_name, (db.Model), table_columns)
+            db.create_all()
             return jsonify({'message': f'Table {table_name} created successfully'}), 200
         except Exception as e:
             return jsonify({'error': str(e)}), 500
-
-    # @staticmethod
-    # def populate_table(request):
